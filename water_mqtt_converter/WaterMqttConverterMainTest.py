5952e63e2300a0ba59c8c4c69933cf7bdeccc78f
water_v2 <water_v2@yunji.ai>
2018-01-02 16:38:59 +0800

    divide three class

--- water_mqtt_converter/WaterMqttConverterMainTest.py
+++ water_mqtt_converter/WaterMqttConverterMainTest.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+处理MqttBridgeRos下发的ros topic，2s上发一次robot status
+'''
+
 import rospy
 import json
 import time
 import random
-import os, sys
+import os
+import sys
 import commands
 import subprocess
 from rospy_message_converter import json_message_converter
@@ -19,24 +24,26 @@ try:
 except ImportError:  # TODO use Threading instead of _thread in python3
     import _thread as thread
 import urllib2
-
 reload(sys)
-sys.setdefaultencoding( "utf-8" )
+sys.setdefaultencoding("utf-8")
+
 
 def json_loads_byteified(json_text):
-    return _byteify(json.loads(json_text, object_hook=_byteify),ignore_dicts=True)
+    return _byteify(json.loads(json_text, object_hook=_byteify), ignore_dicts=True)
 
-def _byteify(data, ignore_dicts = False):
+
+def _byteify(data, ignore_dicts=False):
     if isinstance(data, unicode):
         return data.encode('utf-8')
     if isinstance(data, list):
-        return [ _byteify(item, ignore_dicts=True) for item in data ]
+        return [_byteify(item, ignore_dicts=True) for item in data]
     if isinstance(data, dict) and not ignore_dicts:
         return {
             _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
             for key, value in data.iteritems()
         }
     return data
+
 
 class WaterMqttConverterMain():
     def __init__(self):
@@ -53,27 +60,13 @@ class WaterMqttConverterMain():
         if self.robotid == "":
             rospy.logwarn("product_id is empty")
             return False
-        rospy.Subscriber("/mqtt_bridge/mqtt_topic_go_down", Mqtt_test, self.mqtt_topic_go_down_callback)
-        rospy.Subscriber("/mqtt_bridge/connect_status_test", String, self.mqtt_bridge_connect_status_callback)
-        self.pub_mqtt_go_up = rospy.Publisher('/mqtt_bridge/mqtt_topic_go_up', Mqtt_test, queue_size=10, latch=True)
+        rospy.Subscriber("/mqtt_bridge_ros/mqtt_topic_go_down",
+                         Mqtt_test, self.mqtt_topic_go_down_callback)
+        rospy.Subscriber("/mqtt_bridge_ros/connect_status_test",
+                         String, self.mqtt_bridge_connect_status_callback)
+        self.pub_mqtt_go_up = rospy.Publisher(
+            '/mqtt_bridge_ros/mqtt_topic_go_up', Mqtt_test, queue_size=10, latch=True)
         return True
-
-    def sub_twist(self):
-        rospy.loginfo("sub twist")
-        m = Mqtt_test()
-        #m.name = 'cmd_vel'
-        m.type = 'sub'
-        m.robotid = self.robotid
-        self.twist_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        self.pub_mqtt_go_up.publish(m)
-
-    def sub_download_map_notice(self):
-        rospy.loginfo("sub download map notice")
-        m = Mqtt_test()
-       # m.name = 'robot_map_info'
-        m.type = 'sub'
-        m.robotid = self.robotid
-        self.pub_mqtt_go_up.publish(m)
 
     def init_robot_status(self):
         # get software version
@@ -106,7 +99,7 @@ class WaterMqttConverterMain():
         self.robot_status['hardwareCheck'] = check_result
         '''
 
-        # get lan ip 
+        # get lan wifi
         lan_ip = ''
         srv = rospy.ServiceProxy("/bash_services/wifi", CmdService)
         req = CmdServiceRequest()
@@ -132,18 +125,17 @@ class WaterMqttConverterMain():
         # dummy, wait robot status callback
         self.robot_status['floor'] = 0
         self.robot_status['power'] = 0
-       # self.robot_status['runningState'] = 'error'
+        # self.robot_status['runningState'] = 'error'
         self.robot_status['softwareStop'] = False
         self.robot_status['hardwareStop'] = False
-        self.robot_status['internet'] = 0 # 1:normal,2:not normal, 0:unkonwn
+        self.robot_status['internet'] = 0  # 1:normal,2:not normal, 0:unkonwn
         self.robot_status['notes'] = " "
-        self.robot_status['currentPosition'] = {"name":"", "x":0, "y":0}
+        self.robot_status['currentPosition'] = {"name": "", "x": 0, "y": 0}
         self.robot_status['angle'] = 0
 
-
-
-        #pub robot base info
-        rospy.Subscriber("/robot_status", RobotStatus, self.robot_status_callback)
+        # pub robot base info
+        rospy.Subscriber("/robot_status", RobotStatus,
+                         self.robot_status_callback)
         time.sleep(2)
         while not rospy.is_shutdown():
             if self.mqtt_bridge_connect:
@@ -152,12 +144,12 @@ class WaterMqttConverterMain():
                 m.robotid = self.robotid
                 m.data = json.dumps(self.robot_status)
                 self.pub_mqtt_go_up.publish(m)
-                #rospy.loginfo(m)
+                # rospy.loginfo(m)
             time.sleep(2)
 
     def robot_status_callback(self, data):
         self.robot_status['floor'] = data.current_floor
-        self.robot_status['power'] = data.power_percent 
+        self.robot_status['power'] = data.power_percent
         self.robot_status['softwareStop'] = data.estop_state
         if data.charge_state:
             self.robot_status['charged'] = True
@@ -167,7 +159,6 @@ class WaterMqttConverterMain():
             self.robot_status['workingStatus'] = 2
         else:
             self.robot_status['workingStatus'] = 1
-
 
         '''
         if data.move_status == 'running':
@@ -188,11 +179,6 @@ class WaterMqttConverterMain():
         rospy.loginfo("mqtt_bridge %s", data.data)
         if data.data == "connect":
             self.mqtt_bridge_connect = True
-            time.sleep(0.5)
-            self.sub_twist()
-            time.sleep(0.5)
-            self.sub_download_map_notice()
-            time.sleep(0.5)
             thread.start_new_thread(self.init_robot_status, ())
         elif data.data == "reconnect":
             self.mqtt_bridge_connect = True
@@ -200,42 +186,45 @@ class WaterMqttConverterMain():
             self.mqtt_bridge_connect = False
 
     def mqtt_topic_go_down_callback(self, data):
-        #rospy.loginfo(data)
-        '''
+        # rospy.loginfo(data)
         if data.robotid != self.robotid:
-            rospy.logerr("mismatched robotid(%s<=>%s)", self.robotid, data.robotid)
+            # rospy.logerr("mismatched robotid(%s<=>%s)", self.robotid, data.robotid)
             return
-        '''
-        '''
-        if data.type == 'topic':
-            if data.name == 'cmd_vel':
-                t = json_message_converter.convert_json_to_ros_message('geometry_msgs/Twist', data.data)
-                self.twist_pub.publish(t)
-            elif data.name == 'robot_map_info':
-                self.handle_map_notice(data.data)
-        '''
+
         if data.type == 'move':
             #rospy.INFO("mqtt go down--move:"+data.data)
             print data.data
-            
-            mqtt_data = json_loads_byteified(data.data)
-            urllib2.urlopen("http://localhost:8899/api/move?marker="+mqtt_data["position"])
-            return_data = Mqtt_test()
-            return_data.type = "callback"
-            data_dict = {"cb":mqtt_data['cb'], "status":"20001", "msg":"success"}
-            return_data.data = json.dumps(data_dict)
-            self.pub_mqtt_go_up.publish(return_data)
-            
+            # TODO
+            # 从话题中获得机器人状态依赖发送的频率，肯定有延迟，应该用service
+            if(self.robot_status['workingStatus'] == 2):
+                # robot is running
+                return_data = Mqtt_test()
+                return_data.type = "callback"
+                data_dict = {"cb": mqtt_data['cb'],
+                             "status": "20002", "msg": "working"}
+                return_data.data = json.dumps(data_dict)
+                self.pub_mqtt_go_up.publish(return_data)
+            else:
+                mqtt_data = json_loads_byteified(data.data)
+                urllib2.urlopen(
+                    "http://localhost:8899/api/move?marker=" + mqtt_data["position"])
+                return_data = Mqtt_test()
+                return_data.type = "callback"
+                data_dict = {"cb": mqtt_data['cb'],
+                             "status": "20001", "msg": "success"}
+                return_data.data = json.dumps(data_dict)
+                self.pub_mqtt_go_up.publish(return_data)
+
         if data.type == 'heart':
-            rospy.INFO("mqtt go down--heart:"+data.data)
+            rospy.INFO("mqtt go down--heart:" + data.data)
         if data.type == 'call':
-            rospy.INFO("mqtt go down--call:"+data.data)
+            rospy.INFO("mqtt go down--call:" + data.data)
         if data.type == 'charge':
-            rospy.INFO("mqtt go down--charge:"+data.data)
+            rospy.INFO("mqtt go down--charge:" + data.data)
         if data.type == 'update':
-            rospy.INFO("mqtt go down--update:"+data.data)
+            rospy.INFO("mqtt go down--update:" + data.data)
         if data.type == 'status':
-            rospy.INFO("mqtt go down--status:"+data.data)
+            rospy.INFO("mqtt go down--status:" + data.data)
 
     def handle_map_notice(self, data):
         map_srv = rospy.ServiceProxy("/bash_services/map", CmdService)
@@ -246,6 +235,7 @@ class WaterMqttConverterMain():
             rsp = map_srv(req)
         except Exception as e:
             rospy.logerr("call map service failed, exception:%s" % e)
+
 
 if __name__ == '__main__':
     rospy.init_node('water_mqtt_converter_test')
